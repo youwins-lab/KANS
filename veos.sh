033d97fd352aee11925a5e546acc221981aa5d4b
youwins-lab <95260405+youwins-lab@users.noreply.github.com>
2022-02-04 22:23:51 +0900

    Update veos.sh

--- veos.sh
+++ veos.sh
@@ -6,12 +6,12 @@ hostname leaf1a
 !
 !
 interface Ethernet1
-description EBGP-TO-TOP
-switchport
+description Network10
+no switchport
 ip address 192.168.10.254/24
 !
 interface Ethernet2
-description POD2
+description Network20
 no switchport
 ip address 192.168.20.254/24
 !
@@ -37,23 +37,3 @@ network 10.1.2.0/24
 
 
 
-
-
-
-
-
-neighbor EBGP-TO-TOP peer-group
-neighbor EBGP-TO-TOP remote-as 64512
-neighbor EBGP-TO-TOP send-community
-neighbor IBGP-TO-LEAF-PEER peer-group
-neighbor IBGP-TO-LEAF-PEER remote-as 65001
-neighbor IBGP-TO-LEAF-PEER next-hop-self
-neighbor IBGP-TO-LEAF-PEER send-community
-neighbor EBGP-TO-EDGE peer-group
-neighbor EBGP-TO-EDGE remote-as 65002
-neighbor EBGP-TO-EDGE send-community
-neighbor 1.1.1.2 peer-group IBGP-TO-LEAF-PEER
-neighbor 10.10.10.2 peer-group EBGP-TO-TOP
-neighbor 192.168.1.2 peer-group EBGP-TO-EDGE
-network 10.10.10.0/30
-network 100.100.100.1/32
