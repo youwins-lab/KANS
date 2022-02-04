de42c095d469ee1909d5978b38dffd04e6a752fa
youwins-lab <95260405+youwins-lab@users.noreply.github.com>
2022-02-05 00:36:31 +0900

    Update veos.sh

--- veos.sh
+++ veos.sh
@@ -4,36 +4,42 @@ enable
 configure
 hostname leaf1a
 !
+spanning-tree mode none
+!
+vlan 10
 !
 interface Ethernet1
-description Network10
-no switchport
-ip address 192.168.10.254/24
+	switchport
+	swtichport access vlan 10
+	no shutdown
 !
 interface Ethernet2
-description Network20
-no switchport
-ip address 192.168.20.254/24
+	no switchport
+	no shutdown
+	ip address 192.168.20.254/24
 !
+interface vlan 10
+	no shutdown
+	ip address 192.168.10.254/24
 !
 ip routing
 !
 interface Loopback10
-ip address 10.1.1.254/24
+	ip address 10.1.1.254/24
 !
 interface Loopback20
-ip address 10.1.2.254/24
+	ip address 10.1.2.254/24
+!
+peer-filter K8S-AS-RANGE
+	10 match as-range 64500-64999 result accept
 !
 router bgp 64512
-maximum-paths 4 ecmp 4
-router-id 10.1.1.254
-neighbor 192.168.10.10 remote-as 64512
-neighbor 192.168.20.100 remote-as 64512
-neighbor 192.168.10.101 remote-as 64512
-neighbor 192.168.10.102 remote-as 64512
-network 10.1.1.0/24
-network 10.1.2.0/24
+	router-id 10.1.1.254
+	maximum-paths 4 ecmp 4
+	bgp listen range 192.168.0.0/16 peer-group K8S-PEERS peer-filter K8S-AS-RANGE
+	neighbor K8S-PEERS peer group
+	neighbor K8S-PEERS maximum-routes 12000
+	network 10.1.1.0/24
+	network 10.1.2.0/24
 "
-
-
 
