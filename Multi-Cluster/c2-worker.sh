7cdd37ede0809502c445ecbeeb6c4364e2be30e8
youwins-lab <95260405+youwins-lab@users.noreply.github.com>
2022-03-14 01:17:03 +0900

    change worker config

--- Multi-Cluster/c2-worker.sh
+++ Multi-Cluster/c2-worker.sh
@@ -7,7 +7,7 @@ kubeadm join --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-ve
 
 echo "[TASK 2] Config kubeconfig" 
 mkdir -p $HOME/.kube
-sshpass -p "qwe123" scp -o StrictHostKeyChecking=no root@k8s-m:/etc/kubernetes/admin.conf $HOME/.kube/config >/dev/null 2>&1
+sshpass -p "qwe123" scp -o StrictHostKeyChecking=no root@k8s-c2-m:/etc/kubernetes/admin.conf $HOME/.kube/config >/dev/null 2>&1
 
 echo "[TASK 3] Source the completion"
 source <(kubectl completion bash)
@@ -20,7 +20,7 @@ echo 'complete -F __start_kubectl k' >> /etc/profile
 echo "[TASK 5] Install Cilium Cli"
 curl -s -L --remote-name-all https://github.com/cilium/cilium-cli/releases/download/v0.10.4/cilium-linux-amd64.tar.gz
 tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin >/dev/null 2>&1
-curl -s -L --remote-name-all https://github.com/cilium/hubble/releases/download/v0.9.0/hubble-linux-amd64.tar.gz
-tar xzvfC hubble-linux-amd64.tar.gz /usr/local/bin >/dev/null 2>&1
+#curl -s -L --remote-name-all https://github.com/cilium/hubble/releases/download/v0.9.0/hubble-linux-amd64.tar.gz
+#tar xzvfC hubble-linux-amd64.tar.gz /usr/local/bin >/dev/null 2>&1
 
 echo ">>>> K8S Node config End <<<<"
